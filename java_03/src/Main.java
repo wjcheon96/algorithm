0b0e7ebd77aa19eaa68cc0326dcb03ea6a923df9
wjche <wjcheon96@gmail.com>
2021-12-12 23:21:53 +0900

    codeup

--- java_03/src/Main.java
+++ java_03/src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[] args){
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
 
-        int result= -8>>>3;
-        System.out.println(result);
+        int num;
+        num = sc.nextInt();
+        System.out.println("숫자는 = " + num);
     }
-
 }
